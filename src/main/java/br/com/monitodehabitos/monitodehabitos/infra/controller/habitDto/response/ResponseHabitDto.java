53191717a9910cbd05732157684cf8713d9048e0
matheus mozart <mmsnborges@gmail.com>
2024-10-13 20:19:30 -0300

    resposta comprovando a criação

--- src/main/java/br/com/monitodehabitos/monitodehabitos/infra/controller/habitDto/response/ResponseHabitDto.java
+++ src/main/java/br/com/monitodehabitos/monitodehabitos/infra/controller/habitDto/response/ResponseHabitDto.java
@@ -3,13 +3,17 @@ package br.com.monitodehabitos.monitodehabitos.infra.controller.habitDto.respons
 import br.com.monitodehabitos.monitodehabitos.domain.entities.Habit;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public record ResponseHabitDto(
         Long id,
         String description,
         Boolean done,
         LocalDate start,
-        LocalDate end
+        LocalDate end,
+        List<ProgresDto> progress
+
 ) {
     public ResponseHabitDto(Habit habit) {
         this(
@@ -17,7 +21,10 @@ public record ResponseHabitDto(
         habit.getDescription(),
         habit.getDone(),
         habit.getStart(),
-        habit.getEnd()
+        habit.getEnd(),
+                habit.getProgress().stream()
+                        .map(p -> new ProgresDto(p.getDate(), p.getCompleted()))
+                        .collect(Collectors.toList())
         );
     }
 }
